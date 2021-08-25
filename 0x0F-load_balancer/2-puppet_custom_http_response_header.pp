e054d977cb5e664c1549dfb661d3f97f77752751
tieje <toj320@gmail.com>
2021-08-24 22:31:08 -0400

    0

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,3 +8,68 @@ exec { 'http header':
        sudo service nginx restart',
        provider => shell,
 }
+#automate custom header response
+exec { 'update':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell'
+}
+
+
+#installs nginx
+-> package { 'nginx':
+  ensure   => present,
+  name     => 'nginx',
+  provider => 'apt'
+}
+
+
+#sets index content
+-> file { 'index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => 'Holberton School'
+}
+
+
+#sets 404 contentfile
+-> { '404.html':
+  ensure  => present,
+  path    => '/var/www/html/404.html',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => "Ceci n'est pas une page"
+}
+
+
+#sets config
+-> exec { 'Set default /redirect_me':
+  command  => 'sed -i "48i \\\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}" /etc/nginx/sites-available/default',
+  user     => 'root',
+  provider => 'shell'
+}
+
+
+-> exec { 'Set default /404':
+  command  => 'sed -i "42i \\\n\terror_page 404 /404.html;" /etc/nginx/sites-available/default',
+  user     => 'root',
+  provider => 'shell'
+}
+
+
+-> exec { 'Set X-Served-By':
+  command  => 'sed -i "48i \\\t\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
+  user     => 'root',
+  provider => 'shell'
+}
+
+
+-> exec { 'Start nginx':
+  command  => 'service nginx restart',
+  user     => 'root',
+  provider => 'shell'
+}
