0412433e31899402278e2ce9962d2fa6a0c627eb
tieje <toj320@gmail.com>
2021-09-07 23:51:21 -0400

    trying

--- 0x14-mysql/4-commands.sql
+++ 0x14-mysql/4-commands.sql
@@ -1,7 +1,9 @@
 change master to
     master_host='34.139.127.8',
-    master_user='ubuntu',
+    master_user='replica_user',
     master_password='root',
-    master_log_file='mysql-bin.000003',
+    master_log_file='mysql-bin.000004',
     master_log_pos=154,
-    master_port=3306;
+    master_port=3306,
+    master_server_id,
+    relay_log_file: mysql-bin.
