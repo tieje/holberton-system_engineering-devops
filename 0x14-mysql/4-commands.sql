6dac14bcb8e05464eee336f36c316f0fabfee92a
tieje <toj320@gmail.com>
2021-09-11 14:26:49 -0400

    4

--- 0x14-mysql/4-commands.sql
+++ 0x14-mysql/4-commands.sql
@@ -4,6 +4,11 @@ change master to
     master_password='root',
     master_log_file='mysql-bin.000004',
     master_log_pos=154,
-    master_port=3306,
-    master_server_id,
-    relay_log_file: mysql-bin.
+    master_port=3306;
+-- Justin's answer:
+CHANGE MASTER TO
+MASTER_HOST='<Your Web-01 IP address>',
+MASTER_USER='replica_user',
+MASTER_PASSWORD='<Your replica_user's password>',
+MASTER_LOG_FILE='<Get this from SHOW MASTER STATUS;>',
+MASTER_LOG_POS=<Get this from SHOW MASTER STATUS;>;
